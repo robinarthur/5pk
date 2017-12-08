2bac06d828b8738bfb0d00ac9cd64883f2bfbcbd
robinarthur <stukka@gmx.de>
2017-12-08 17:19:57 +0100

    fill_the_db

--- txt_edit/db/CREATE_5pk.sql
+++ txt_edit/db/CREATE_5pk.sql
@@ -20,3 +20,29 @@ CREATE TABLE IF NOT EXISTS `tbl_Sentences` (
 );
 CREATE INDEX ind_Sentences_Sentence_ID ON tbl_Sentences(Sentence_ID)
 COMMIT;
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
+
+def create_table(conn, create_table_sql)
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
