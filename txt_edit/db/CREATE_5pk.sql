33f95bbd19dbf30c94d933cd0b7453e2561bb2f3
robinarthur <stukka@gmx.de>
2017-12-11 12:54:16 +0100

    create the books db

--- txt_edit/db/CREATE_5pk.sql
+++ txt_edit/db/CREATE_5pk.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `tbl_Authors` (
 	`Author_ID`		INTEGER Primary KEY,
 	`Author_NAME`	TEXT NOT NULL
 );
-CREATE INDEX ind_Authors_Author_ID ON tbl_Authors(Author_ID)
+CREATE INDEX ind_Authors_Author_ID ON tbl_Authors(Author_ID);
 CREATE TABLE IF NOT EXISTS `tbl_Books` (
 	`Book_ID`		INTEGER Primary KEY,
 	`Book_NAME`		TEXT NOT NULL,
@@ -11,14 +11,14 @@ CREATE TABLE IF NOT EXISTS `tbl_Books` (
 	`Ỳear` 			TEXT NOT NULL,
 	FOREIGN KEY(Author_ID) REFERENCES tbl_Authors(Author_ID)
 );
-CREATE INDEX ind_Books_Book_ID ON tbl_Books(Book_ID)
+CREATE INDEX ind_Books_Book_ID ON tbl_Books(Book_ID):
 CREATE TABLE IF NOT EXISTS `tbl_Sentences` (
 	`Sentence_ID`	INTEGER PRIMARY KEY,
 	`Book_ID`		INTEGER NOT NULL,
 	`Sentence`		TEXT NOT NULL,
 	FOREIGN KEY(Book_ID) REFERENCES tbl_Books(Book_ID)
 );
-CREATE INDEX ind_Sentences_Sentence_ID ON tbl_Sentences(Sentence_ID)
+CREATE INDEX ind_Sentences_Sentence_ID ON tbl_Sentences(Sentence_ID);
 COMMIT;
 
 def create_connection(db_file):
